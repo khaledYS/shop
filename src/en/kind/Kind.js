205d5423cbe57c4bd5ba9dd3373fdc0dd7ac7d7b
AmmarYasser <admin@ammar.awash>
2021-08-11 22:03:54 +0300

    i just did some updates in Kind.js and iam going to upload this in case this fails up

--- src/en/kind/Kind.js
+++ src/en/kind/Kind.js
@@ -1,38 +1,35 @@
-import { useRef, useState } from 'react';
+import { useRef, useEffect} from 'react';
 import './kind.css'
 
 const Kind = ({prop}) => {
 
+    // defining the ref
+    const kindRef = useRef('')
 
+    useEffect(()=>{
 
-    // defining the container to give every child an onclick event 
-    const kindContainer = useRef();
-    console.log(kindContainer.current);
-    // i'll shortcut the the name via creating another variable like this from kindContainer.current to kindCon and get all of the children in this vaiable
-    const kindKids = kindContainer.current.querySelectorAll('.kind');
-    console.log(kindKids);
-    // then here we give a func to every child
-    kindKids.forEach(element => {
-        element.onClick = ()=>{
-            // this varaible contains the data-kind attribute 
-            let kindAttr = element.getAttribute('data-kind');
+        kindRef.current.querySelectorAll('.kind').forEach(element => {
 
-            prop.changeOption({key:'kind', value:kindAttr})
-            prop.setCurrentWindow(kindAttr)
-        }
-    });
+            element.onclick = (e)=>{
+                const kind = e.target.getAttribute('data-kind');
+                prop.changeOption({key:'kind', value:kind});
+                prop.setCurrentWindow(kind)
+            }
 
+        });
+
+    })
 
 
     return (
-    <div className="kind-container" ref={kindContainer} >
+    <div ref={kindRef} className="kind-container" >
         <div className="kind" data-kind='drink'>Drink</div>
         <div className="kind" data-kind='sweet'>Sweet</div>
         <div className="kind" data-kind='lorem'>lorem</div>
         <div className="kind" data-kind='ipsum'>ipsum</div>
-        <div className="kind" data-kind='ipsum'>food</div>
+        <div className="kind" data-kind='food' >food</div>
     </div>
     );
 }
- 
+
 export default Kind;
